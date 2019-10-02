33068f3b06ae7b3053efbda891903c6319a2369b
andrew stehno <andrew_stehno@yahoo.com>
2019-10-02 15:20:16 -0600

    created sign up/logout/login for firebase

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -11,8 +11,59 @@ var firebaseConfig = {
 // Initialize Firebase:
 firebase.initializeApp(firebaseConfig);
 
-// setup materialize components for modals:
-document.addEventListener('DOMContentLoaded', function() {
+const auth = firebase.auth();
+const database = firebase.database();
+
+// Signup users:
+const signUpForm = document.querySelector('#signup-form');
+signUpForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // get user info:
+  const email = signUpForm['signup-email'].value;
+  const password = signUpForm['signup-password'].value;
+
+  // Sign up user:
+  auth.createUserWithEmailAndPassword(email, password).then(cred => {
+    console.log(cred);
+
+    const modal = document.querySelector('#modal-signup');
+    M.Modal.getInstance(modal).close();
+    signUpForm.reset();
+  })
+})
+
+// Log user out:
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+  e.preventDefault();
+  auth.signOut().then(() => {
+    console.log("user has signed out");
+  })
+});
+
+// log user in:
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // get user info:
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value;
+
+  auth.signInWithEmailAndPassword(email, password).then(cred => {
+    console.log(cred);
+    const modal = document.querySelector('#modal-login')
+    M.Modal.getInstance(modal).close();
+    signUpForm.reset();
+  })
+});
+
+
+
+
+// Setup materialize components for modals:
+document.addEventListener('DOMContentLoaded', function () {
 
   let modals = document.querySelectorAll('.modal');
   M.Modal.init(modals);
