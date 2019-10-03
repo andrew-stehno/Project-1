3883d243eeffdee356c1b310b13b0e0e08efafd0
Tyler Stewart <tyler.stewart1026@gmail.com>
2019-10-03 12:49:57 -0600

    added the drop down bar parameters, and incorporated them into the search query for API, working

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -196,31 +196,46 @@ $(document).ready(function(){
   $('.sidenav').sidenav();
 });
 
+// makinf Array to hold dropdown menu items and looping through to dynamically create the options. 
+var optionsArr = ["Balanced", "Gluten-free", "Vegetarian", "Keto", "Paleo", "Vegan", "Pescetarian", "Dairy-free"]
 
+for (let i = 0; i < optionsArr.length; i++) {
+  // test loop
+  console.log(optionsArr[i]);
+  // var that holds new options
+  var newOpt = $('<option>')
+
+  newOpt.text(optionsArr[i]);
+
+  $('.optionsField').append(newOpt);
+
+};
 
 
 // create search var
 
 $(document).on("click", ".keywordSearch", function(){
 var search = $('.keyWord').val().trim();
-var queryUrl = "https://api.edamam.com/search?q=" + search + "&app_id=$42a05216&app_key=$ddaf66796324f3322e79ef209fccf704"
+var dietParam = $('.dietParamSelector').val();
+var queryUrl = "https://api.edamam.com/search?q=" + search + "&health=" + dietParam + "&app_id=$42a05216&app_key=$ddaf66796324f3322e79ef209fccf704&from=0&to=10"
   console.log("clicked");
   console.log(search);
+  console.log(dietParam);
 
 
   // reset search bar 
   $(".keyWord").val("");
+
+
+
+
+$.ajax({
+   url: queryUrl,
+   method: "GET"
+}).then(function(response){
+   console.log(response);
 });
-
-
-
-//$.ajax({
-  //  url: queryUrl,
-   // method: "GET"
-//}).then(function(response){
-  //  console.log(response);
-//});
-
+});
 
 // API Key
 // https: //api.edamam.com/search?q=chicken?
